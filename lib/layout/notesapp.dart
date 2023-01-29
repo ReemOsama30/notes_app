e78145257360bdc60e2389ade7bad5d2c1717393
Reem Osama <reem.osama3052001@gmail.com>
2023-01-29 21:41:31 +0200

    Show ModalBottomSheet

--- lib/layout/notesapp.dart
+++ lib/layout/notesapp.dart
@@ -1,14 +1,23 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../widgets/add_note_bottom_sheet.dart';
 import '../widgets/note_app_body.dart';
 
 class NotesApp  extends StatelessWidget {
+  const NotesApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: (){},child:const Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+
+        showModalBottomSheet(context: context, builder: (context){
+
+          return const AddNoteBottomSheet();
+        });
+      },child:const Icon(Icons.add),),
       body:const NotesAppBody()
 
     );
   }
 }
+
