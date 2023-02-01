cd87ddb031da0faedf2e53451aa696e5351011a5
Reem Osama <reem.osama3052001@gmail.com>
2023-02-01 11:36:24 +0200

    Add Hive Package

--- lib/model/note_model.dart
+++ lib/model/note_model.dart
@@ -5,5 +5,10 @@ final String subtitle;
 final String date;
 final int color;
 
-  NoteModel({required this.title,required this.color,required this.date,required this.subtitle});
+  NoteModel({
+    required this.title
+    ,required this.color,
+    required this.date,
+    required this.subtitle
+  });
 }
